6aab5374dab285a0b6228a5244b06c411476784f
apipshato <afifshatos@gmail.com>
2020-11-18 15:13:28 +0700

    add models , getuser media  add event

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 // const { Stream } = require("stream")
 
-const video = document.getElementById('video')
+const video = document.getElementById("video");
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandMarl68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-]).then(startVideo)
+  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+]).then(startVideo);
 
-function startVideo(){
+function startVideo() {
   navigator.getUserMedia(
-    { video : {} },
-    stream => video.srcObject =stream,
-    err => console.error(err)
-  )
+    { video: {} },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
 }
-startVideo()
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  const displaySize = {
+    width: video.width,
+    height: video.height,
+  };
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    console.log(detections);
+  }, 100);
+});
